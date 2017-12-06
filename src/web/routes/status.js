b864c441ab65ea6947bb91af6553ee84919cbc35
ShinCurry <shincurryyang@gmail.com>
2017-12-06 10:40:44 +0800

    add status page

--- src/web/routes/status.js
+++ src/web/routes/status.js
@@ -1,37 +1,21 @@
 const async = require('async');
 const express = require('express');
-const request = require('request');
 
 const router = express.Router();
-const config = require('../../../config.js');
 const Manager = require('../../core/manager.js');
 
 router.get('/', (req, res, next) => {
-  async.series({
+  async.parallel({
     seriesCount: Manager.getSeriesesCount,
     episodesCount: Manager.getEpisodesCount,
     loopsCount: Manager.getLoopsCount,
-    jobs: (callback) => {
-      const app = config.automator.app;
-      request(`http://${app.auth.username}:${app.auth.password}@${app.host}:${app.port}${app.url}/jobs/0..100/desc`, (error, response, body) => {
-        if (error) {
-          callback(null, { active: [], completed: [] });
-          return;
-        }
-        const jobs = JSON.parse(body);
-
-        callback(null, {
-          active: jobs.filter(job => job.state === 'active').slice(0, 10),
-          completed: jobs.filter(job => job.state === 'complete' && job.type === 'upload').slice(0, 10),
-        });
-      });
-    },
   }, (err, status) => {
     if (err) {
       next();
       return;
     }
     res.render('status', {
+      pageType: 'status',
       status,
     });
   });
