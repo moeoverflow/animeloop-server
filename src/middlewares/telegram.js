faffa9c09a0c5bee557d45df402647d36c316005
ShinCurry <shincurryyang@gmail.com>
2017-12-18 16:17:08 +0800

    fix telegram middleware

--- src/middlewares/telegram.js
+++ src/middlewares/telegram.js
@@ -1,13 +1,15 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
+const config = require('../../config.js');
 
 router.use('/:id', (req, res, next) => {
   const id = req.params.id;
   const ua = req.get('User-Agent');
 
   if (ua.indexOf('TelegramBot') > -1) {
-    const fileName = `/home/shin/animeloop_library/mp4_720p/${id}.mp4`;
+    const fileName = path.join(config.storage.dir.data, 'mp4_720p', `${id}.mp4`);
     res.type('video/mp4');
     res.sendFile(fileName, next);
   } else {
